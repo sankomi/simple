b30c49d06c6dc67fa23bc62e908f1139d12f4c71
sanko <sang.koh@hotmail.com>
2024-07-22 21:46:03 +1000

    replace with object properties

--- simple.js
+++ simple.js
@@ -5,15 +5,37 @@ function treeify(element) {
 
 	const texts = textNodes.map(node => {
 		const content = node.textContent;
-		const placeholders = content.match(/{{[a-z0-9]*}}/g)
+		const placeholders = content.match(/{{[a-z0-9]+}}/g)
 			?.map(match => match.replaceAll(/({{)|(}})/g, ""))
 			|| [];
+
+		if (placeholders.length === 0) return null;
 		return {node, content, placeholders};
-	});
+	})
+		.filter(text => text !== null);
+
+	const propertys = textNodes.map(node => {
+		const content = node.textContent;
+		const placeholders = new Map();
+		content.match(/{{[a-z0-9]+\.[a-z0-9]+}}/g)
+			?.map(match => match.replaceAll(/({{)|(}})/g, ""))
+			?.forEach(match => {
+				const split = match.split(".");
+				const name = split[0];
+
+				if (!placeholders.has(name)) placeholders.set(name, []);
+				const array = placeholders.get(name);
+				array.push(split[1]);
+			});
+
+		if (placeholders.size === 0) return null;
+		return {node, content, placeholders};
+	})
+		.filter(property => property !== null);
 
 	const subtrees = elementNodes.map(element => treeify(element));
 
-	return {element, texts, subtrees};
+	return {element, texts, propertys, subtrees};
 }
 
 function replace(tree, data) {
@@ -22,10 +44,29 @@ function replace(tree, data) {
 
 		text.placeholders.forEach(placeholder => {
 			const value = data[placeholder] ?? undefined;
-			textContent = text.content.replaceAll(`{{${placeholder}}}`, value);
+			textContent = textContent.replaceAll(`{{${placeholder}}}`, value);
 		});
 
 		text.node.textContent = textContent;
+	});
+
+	tree.propertys.forEach(property => {
+		let textContent = property.content;
+
+		property.placeholders.forEach((keys, name) => {
+			const object = data[name] ?? undefined;
+			if (object === undefined) {
+				textContent = textContent.replaceAll(new RegExp(`{{${name}\.[a-z0-9]+}}`, "g"), "undefined");
+				return;
+			}
+
+			keys.forEach(key => {
+				const value = object[key] ?? undefined;
+				textContent = textContent.replaceAll(new RegExp(`{{${name}\.${key}}}`, "g"), value);
+			});
+		});
+
+		property.node.textContent = textContent;
 	});
 
 	tree.subtrees.forEach(subtree => replace(subtree, data));
@@ -34,6 +75,12 @@ function replace(tree, data) {
 function hasPlaceholder(tree, placeholder) {
 	for (const text of tree.texts) {
 		if(text.placeholders.includes(placeholder)) {
+			return true;
+		}
+	}
+
+	for (const property of tree.propertys) {
+		if (property.placeholders.has(placeholder)) {
 			return true;
 		}
 	}
